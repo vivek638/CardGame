42be78cca43cdd7d72ee5fd0f29ccbb3998370c8
Vivek Kumar Soni <43683480+vivek638@users.noreply.github.com>
2018-11-08 22:23:13 +0530

    Add files via upload

---
+++ CardGame/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardGame
+{
+    class Card
+    {
+        private string face;
+        private string suit;
+
+        public Card(string cardFace, string cardSuit)
+        {
+            face = cardFace;
+            suit = cardSuit;
+        }
+
+        public override string ToString()
+        {
+            return face + "of" + suit;
+        }
+    }
+}
---
+++ CardGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck1 = new Deck();
+            deck1.Shuffle();
+            for (int i = 0; i < 52; i++)
+            {
+                Console.WriteLine("{0,-19}", deck1.DealCard());
+                if ((i + 1) % 4 == 0)
+                    Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
